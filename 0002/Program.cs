ac812c49ee69b27af847ddbfe4f325142b392112
motooka <kouma.motooka@tplg.co.jp>
2017-06-13 16:11:31 +0900

    add: 0002 Digit Number

---
+++ 0002/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+/*
+ * http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=0002
+ */
+namespace _0002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetOut(new StreamWriter(Console.OpenStandardOutput()) { AutoFlush = false });
+
+            string line;
+            while ((line = Console.ReadLine()) != null)
+            {
+                string[] tokens = line.Split(' ');
+                int a = int.Parse(tokens[0]);
+                int b = int.Parse(tokens[1]);
+
+                Console.WriteLine(Digit(a+b));
+            }
+
+            Console.Out.Flush();
+        }
+
+        static public int Digit(int num)
+        {
+            // Mathf.Log10(0)はNegativeInfinityを返すため、別途処理する。
+            return (num == 0) ? 1 : ((int)Math.Log10(num) + 1);
+        }
+    }
+}
