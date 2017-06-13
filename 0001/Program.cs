40eb63aa6f28ac83715fa441f9e77936fd37a5c1
motooka <kouma.motooka@tplg.co.jp>
2017-06-13 15:56:45 +0900

    add: 0001_List of Top 3 Hills

---
+++ 0001/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+/*
+ * http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=0001
+ */
+namespace _0001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputs = new int[10];
+            for(int i=0; i<10; i++)
+            {
+                inputs[i] = int.Parse(Console.ReadLine());
+            }
+
+            Array.Sort(inputs);
+            Array.Reverse(inputs);
+
+            for(int i=0; i<3; i++)
+            {
+                Console.WriteLine(inputs[i]);
+            }
+            
+        }
+    }
+}
